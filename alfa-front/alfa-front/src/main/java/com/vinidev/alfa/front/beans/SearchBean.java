40e806f2495cf0b4c1eff9aca0a13ba931d92306
viniberaldo <v.a.beraldo@gmail.com>
2023-11-05 16:05:24 -0300

    projeto jsf quase pronto

--- alfa-front/alfa-front/src/main/java/com/vinidev/alfa/front/beans/SearchBean.java
+++ alfa-front/alfa-front/src/main/java/com/vinidev/alfa/front/beans/SearchBean.java
@@ -1,18 +1,24 @@
 package com.vinidev.alfa.front.beans;
 
-import javax.faces.application.FacesMessage;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.bean.SessionScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.inject.Named;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-@Named("bean")
-@RequestScoped
+@ManagedBean(name = "SearchBean", eager = true)
+@SessionScoped
 public class SearchBean {
 
+    private static final Logger LOGGER = Logger.getLogger(SearchBean.class.getName());
     private String searchString;
     private String razaoSocial;
     private String cidade;
@@ -21,21 +27,17 @@ public class SearchBean {
     private String endereco;
     private String telefone;
 
+    @PostConstruct
+    public void init() {
+        this.searchString = "";
+    }
+
     public String getSearchString() {
         return searchString;
     }
 
-    public String setSearchString() {
-        // Get the value entered in the text field
-        String searchString = FacesContext.getCurrentInstance().
-                getExternalContext().getRequestParameterMap().
-                get("searchString");
-
-        // Set the searchString property
+    public void setSearchString(String searchString) {
         this.searchString = searchString;
-
-        // Redirect to the second page
-        return "search.xhtml?faces-redirect=true";
     }
 
     public String getRazaoSocial() {
@@ -86,45 +88,62 @@ public class SearchBean {
         this.telefone = telefone;
     }
 
-    public void search() {
+    public void search(String searchString) throws MalformedURLException, IOException {
+        LOGGER.log(Level.INFO, "Searching for: {0}", searchString);
         String endpoint = "http://localhost:8080/api-data/" + searchString;
 
-        Client client = ClientBuilder.newClient();
-        String apiResponse = client.target(endpoint)
-                .request(MediaType.APPLICATION_JSON)
-                .get(String.class);
+        URL url = new URL(endpoint);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Accept", "application/json");
 
-        // Process the API response
-        if (apiResponse != null && !apiResponse.isEmpty()) {
-            String[] responseParts = apiResponse.split(", ");
-            for (String part : responseParts) {
-                String[] keyValue = part.split(": ");
-                if (keyValue.length == 2) {
-                    String key = keyValue[0].trim();
-                    String value = keyValue[1].trim();
+        int status = con.getResponseCode();
+        if (status == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
 
-                    switch (key) {
-                        case "Razao Social":
-                            razaoSocial = value;
-                            break;
-                        case "Cidade":
-                            cidade = value;
-                            break;
-                        case "Situacao Cadastral":
-                            situacao = value;
-                            break;
-                        case "Data Situacao Cadastral":
-                            data = value;
-                            break;
+            // Process the API response
+            String apiResponse = content.toString();
+            if (apiResponse != null && !apiResponse.isEmpty()) {
+                LOGGER.log(Level.INFO, "API response: {0}", apiResponse);
+                String[] responseParts = apiResponse.split(", ");
+                for (String part : responseParts) {
+                    String[] keyValue = part.split(": ");
+                    if (keyValue.length == 2) {
+                        String key = keyValue[0].trim();
+                        String value = keyValue[1].trim();
+
+                        switch (key) {
+                            case "Razao Social" ->
+                                setRazaoSocial(value);
+                            case "Cidade" ->
+                                setCidade(value);
+                            case "Situacao Cadastral" ->
+                                setSituacao(value);
+                            case "Data Situacao Cadastral" ->
+                                setData(value);
+                        }
                     }
                 }
+                LOGGER.log(Level.INFO, "Updated properties: " + this.cidade);
+                // Redirect to search.xhtml after processing the API response
+                FacesContext.getCurrentInstance().getExternalContext().redirect("search.xhtml?faces-redirect=true");
+            } else {
+                // Handle empty or invalid response
+                FacesContext.getCurrentInstance().addMessage(
+                        null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                                "Error", "Invalid response from API"));
             }
         } else {
-            // Handle empty or invalid response
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Invalid response from API"));
+            // Handle error status code
+            FacesContext.getCurrentInstance().addMessage(
+                    null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            "Error", "Failed to call API: " + status));
         }
-
-        client.close();
     }
 
     public void save() {
